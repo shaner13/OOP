7a7159f59190ea834519b6be9acdec2c0cc8c0da
Shane <shaneriedy5@gmail.com>
2019-09-29 02:07:33 +0100

    lab shane

---
+++ Week 2/binary_to_decimal.py
@@ -0,0 +1,18 @@
+#initialising variables
+binary = input("Enter a number in binary please.\n")
+length = len(binary) - 1
+decimal=0
+i=0
+
+while(i<=length):
+
+    decimal = decimal + int(binary[i]) * 2 ** (length-i)
+    i = i + 1
+
+#end while
+
+#printing number in decimal
+print("Your number in decimal is:\n")
+print(decimal)
+
+
---
+++ Week 2/decimal_to_binary.py
@@ -0,0 +1,47 @@
+#intiialising variables
+
+decimal_num = 0
+correct = 0
+i = 0
+binary = ''
+
+
+while(correct==0):
+
+    decimal_num = int(input("Enter a number to be converted to binary.\n"))
+
+    if(decimal_num>=0):
+
+        correct = correct + 1
+
+     #end if
+
+    else:
+
+        print("Enter a positive integer please.")
+
+    #end else
+#end while
+
+while(decimal_num>0):
+
+    if(decimal_num % 2 == 0):
+
+        binary = binary + '0'
+    #end if
+
+    else:
+
+        binary = binary + '1'
+
+    #end else
+
+    decimal_num = decimal_num // 2
+
+#end while
+
+print("Your number in binary is:\n")
+
+print(binary[::-1])
+
+#end for
