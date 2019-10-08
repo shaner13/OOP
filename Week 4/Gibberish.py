1474ca177e3fe6966026423496b376526f231b33
Shane <shaneriedy5@gmail.com>
2019-10-08 19:25:28 +0100

    lab shane

--- Week 4/Gibberish.py
+++ Week 4/Gibberish.py
@@ -7,44 +7,109 @@
 #into every syllable, (each new vowel will be considered a syllable).
 #Before the program exits the user will be asked if they would like to play again.
 
-import string
+#Creating File
 
-#Initialising Variables
+filename = input("Please name your file.\n")
+filename = filename + '.txt'
+filename_length = len(filename)
+input_file = open(filename,"w")
+print("Honestly, the story is complete gibberish,with only the kind of goofy logic a child would come up with.\n",file=input_file)
+output_file = filename[:filename_length-4] + 'Gibberish' + '.txt'
+output_file = open(output_file,"a+")
 
-vowels = "aeiouAEIOU"
-gibberish_string = ''
+#Explaining rules
 
-first_vowel_complete = False
-second_vowel_complete = False
+print("Hello! Welcome to the Gibberish game!\nYou will be asked for two gibberish infixes\n"
+        "then you will be asked for a word to be translated to gibberish using your vowels.\n"
+        "The infix will be inserted after each vowel, but not for consecutive vowels.\n"
+        "If you would like you may use the '*' wildcard at the start of your syllable to add the vowel"
+        "to your gibberish\n"
+        "syllable. If your word is longer than two syllables it will repeatedly add the second\n"
+        "gibberish infix you provided before each vowel.\nLastly you will asked if you would"
+        "like to play again or exit the game once your word has been translated.\nEnjoy!\n")
 
-#Getting user input and explaining rules.
+play = 'y'
 
-print("Hello! Welcome to the Gibberish game!\n You will be asked for two gibberish infixes\n"
-      "then you will be asked for a word to be translated to gibberish using your vowels.\n"
-      "The infix will be inserted after each vowel, but not for consecutive vowels.\n"
-      "If you would like you may use the '*' wildcard at the start of your syllable to add the vowel"
-      "to your gibberish\n"
-      "syllable. If your word is longer than two syllables it will repeatedly add the second\n"
-      "gibberish infix you provided before each vowel.\n Lastly you will asked if you would"
-      "like to play again or exit the game once your word has been translated.\n Enjoy!\n")
+while(play == 'y' or play == 'yes'):
 
-user_syllable_1 = input("Please enter a syllable.\n")
-user_syllable_2 = input("Please enter another syllable.\n")
-user_word = input("Finally, please enter a word to be translated into gibberish.\n")
+    #Initialising Variables
 
-#Converting Word to gibberish.
+    vowels = "aeiouAEIOU"
+    gibberish_string = ''
+    first_vowel_complete = False
 
-#ib ob
-#shaners
-#sh ib a n ob e rs
 
-for i,char in enumerate(user_word):
-    if char in vowels:
-        if first_vowel_complete == False:
-            gibberish_string = user_word[i-1] + user_syllable_1 + char
-            first_vowel_complete = True
+    #Getting user input
 
-print(gibberish_string)
+    user_syllable_1 = input("Please enter a syllable.\n")
+    user_syllable_2 = input("Please enter another syllable.\n")
+    user_word = input("Finally, please enter a word to be translated into gibberish.\n")
+    length = len(user_word)
+
+    #Converting Word to gibberish.
+
+    for i,char in enumerate(user_word):
+        if char in vowels:
+            if first_vowel_complete == False:
+                if user_syllable_1[0] == '*':
+                    if i == length-2:
+                        gibberish_string = user_word[0:i] + char + user_syllable_1[1:] + char + user_word[i+1:i+2]
+                        j = i
+                        first_vowel_complete = True
+                    #end if
+                    else:
+                        gibberish_string = user_word[:i] + char + user_syllable_1[1:] + char
+                        j = i
+                        first_vowel_complete = True
+                    #end else
+                #end if
+                else:
+                    if i == length-2:
+                        gibberish_string = user_word[0:i] + user_syllable_1 + char + user_word[i+1:i+2]
+                        j = i
+                        first_vowel_complete = True
+                    #end if
+                    else:
+                        gibberish_string = user_word[:i] + user_syllable_1 + char
+                        j = i
+                        first_vowel_complete = True
+                    #end else
+                #end else
+            #end if
+            else:
+                if user_syllable_2[0] == '*':
+                    if user_word[i-1] not in vowels:
+                        if user_word[length-2] not in vowels:
+                            gibberish_string = gibberish_string + user_word[j:i] + char + user_syllable_2[1:] + char + user_word[i+1:length]
+                            j = i
+                        #end if
+                        else:
+                            gibberish_string = gibberish_string + user_word[j:i] + char + user_syllable_2[1:] + char
+                            j= i
+                        #end else
+                    #end if
+
+                else:
+                    if user_word[i-1] not in vowels:
+                        if user_word[length-2] not in vowels:
+                            gibberish_string = gibberish_string + user_word[j:i] + user_syllable_2 + char + user_word[i+1:length]
+                            j = i
+                        #end if
+                        else:
+                            gibberish_string = gibberish_string + user_word[j:i] + user_syllable_2 + char
+                            j = i
+                        #end else
+                    #end if
+                #end else
+            #end else
+         #end if
+    #end for
+
+    print("Your word in gibberish is:",gibberish_string)
+    print(gibberish_string,file=output_file)
+    play = input("Would you like to play again? y/n\n")
+
+#end while
 
 
 
